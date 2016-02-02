626521c5d6cd76d30908ca2d73c1a5e167f18b2b
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2016-02-02 17:40:31 -0500

    working example

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'css/example.css': 'sass/example.scss'
+        }
+      }
+    },
+
+    watch: {
+      files: ['sass/example.scss'],
+      tasks: ['sass'],
+      options: {
+        livereload: true
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+
+};
